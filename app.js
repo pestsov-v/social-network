265c5874664c95a741006504fd26a2946dfe0af4
Vladislav Pestsov <pestsov.js@gmail.com>
2021-10-03 21:26:42 +0300

    Install and require express and pug
    created server and first pug tamplate

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const http = require('http')
+const express = require('express');
+
+const app = express();
+const PORT = 3003;
+const serverFunction = () => console.log(`Server listening on port ${PORT}`)
+
+const server = app.listen(PORT, serverFunction);
+
+app.set("view engine", "pug")
+app.set("views", "views")
+
+app.get("/", (req, res, next) => {
+    res.status(200).render("home.pug");
+})
