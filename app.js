21f742ea42712651c5bdc5d59e49ed06b2ab7a8e
Vladislav Pestsov <pestsov.js@gmail.com>
2021-10-03 21:52:06 +0300

    created middleware requireLogin
    This middleware redirect user on login page if this user doesn`t has a session

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const middleware = require('./middleware');
 
 const app = express();
 const PORT = 3003;
-const serverFunction = () => console.log(`Server listening on port ${PORT}`)
+const serverFunction = () => console.log(`Server listening on port ${PORT}`);
 
 const server = app.listen(PORT, serverFunction);
 
-app.set("view engine", "pug")
-app.set("views", "views")
+app.set("view engine", "pug");
+app.set("views", "views");
 
-app.get("/", (req, res, next) => {
+app.get("/", middleware.requireLogin, (req, res, next) => {
 
     const payload = {
         pageTitle: "Home"
