353bad206fc875cfe8f75f34939296158d891b57
Vladislav Pestsov <pestsov.js@gmail.com>
2021-10-04 19:17:02 +0300

    created a post metod to register route

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const bodyParser = require('body-parser');
 const middleware = require('./middleware');
 const loginRoute = require('./routes/loginRoutes');
-const registerRoutes = require('./routes/registerRoutes')
+const registerRoutes = require('./routes/registerRoutes');
 
 const app = express();
 const PORT = 3003;
@@ -12,10 +13,11 @@ const server = app.listen(PORT, serverFunction);
 
 app.set("view engine", "pug");
 app.set("views", "views");
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/login", loginRoute)
-app.use("/register", registerRoutes)
+app.use("/login", loginRoute);
+app.use("/register", registerRoutes);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
 
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -1,14 +1,39 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const e = require('express');
 const app = express();
 const router = express.Router();
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false}))
+
 
 router.get("/", (req, res, next) => {
     res.status(200).render("register.pug");
 })
+
+router.post("/", (req, res, next) => {
+    
+    const firstName = req.body.firstName.trim();
+    const lastName = req.body.lastName.trim();
+    const username = req.body.username.trim();
+    const email = req.body.email.trim();
+    const password = req.body.password;
+
+    const payload = req.body;
+    
+
+    if (firstName && lastName && username && email && password) {
+        
+    } else {
+        payload.errorMessage = "Заполните все поля";
+        res.status(200).render("register.pug", payload);
+    }
+
+    
+})
+
 
 module.exports = router;
