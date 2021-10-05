9be861f603d2717685b5df7214701b8bc8839c11
Vladislav Pestsov <pestsov.js@gmail.com>
2021-10-05 23:01:22 +0300

    created css styles for postFooter

--- public/js/common.js
+++ public/js/common.js
@@ -47,7 +47,7 @@ function createPostHTML(postData) {
                     </div>
                     <div class='postContentContainer'>
                         <div class='header'>
-                            <a href='/profile/${postedBy.username}'>${displayName}</a>
+                            <a href='/profile/${postedBy.username}' class='displayName'>${displayName}</a>
                             <span class='username'>@${postedBy.username}</span>
                             <span class='date'>${timestamps}</span>
                         </div>
@@ -55,7 +55,21 @@ function createPostHTML(postData) {
                             <span>${postData.content}</span>
                         </div>
                         <div class='postFooter'>
-                            
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='far fa-comment'></i>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='fas fa-retweet'></i>
+                                </button>
+                            </div>
+                            <div class='postButtonContainer'>
+                                <button>
+                                    <i class='far fa-heart'></i>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
