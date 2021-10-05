6a8e6bf133847de6a79cedc6e72c5863eee9494c
Vladislav Pestsov <pestsov.js@gmail.com>
2021-10-05 21:31:42 +0300

    created disabled function in submitButton

---
+++ public/js/common.js
@@ -0,0 +1,17 @@
+$("#postTextarea").keyup(event => {
+    const textbox = $(event.target)
+    const value = textbox.val().trim()
+    
+    const submitButton = $("#submitPostButton");
+
+    if (submitButton.length == 0) {
+        return alert ("no submit button found");
+    };
+
+    if (value == "") {
+        submitButton.prop("disabled", true);
+        return;
+    }
+
+    submitButton.prop("disabled", false);
+})
--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -30,7 +30,7 @@ const UserSchema = new Schema({
     },
     profilePic: {
         type: String,
-        default: "/images/profilePic.png"
+        default: "/images/profilePic.jpg"
     }
 }, {timestamps: true})
 
