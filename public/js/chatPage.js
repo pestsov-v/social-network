1589994c766c6b314228c1bcab50cad3c55adaa7
Vladislav Pestsov <pestsov.js@gmail.com>
2021-11-04 23:37:45 +0300

    showing the typing dots gif when user is typing

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -1,9 +1,7 @@
 $(document).ready(() => {
 
     socket.emit("join room", chatId);
-    socket.on("typing", () => {
-        console.log("user is typing")
-    })
+    socket.on("typing", () => $(".typingDots").show())
 
     $.get(`/api/chats/${chatId}`, (data) => $("#chatName").text(getChatName(data)))
 
