fa3a134726f0f6665f854c07664d637e82b90d3e
Vladislav Pestsov <pestsov.js@gmail.com>
2021-11-06 20:07:49 +0300

    Creating the notifications html

--- public/js/notificationsPage.js
+++ public/js/notificationsPage.js
@@ -1,5 +1,30 @@
 $(document).ready(() => {
     $.get("/api/notifications", (data) => {
-        console.log(data)
+        outputNotificationList(data, $(".resultsContainer"))
     })
 })
+
+function outputNotificationList(notifications, container) {
+    notifications.forEach(notification => {
+        const html = createNotificationHtml(notification)
+        container.append(html)
+    })
+
+    if (notifications.length == 0) {
+        container.append("<span class='NoResults'>У вас нет никаких уведомлений</span>")
+    }
+}
+
+function createNotificationHtml(notification) {
+
+    const userFrom = notification.userFrom;
+
+    return `<a href='#' class='resultListItem notification'>
+                <div class='resultsImageContainer'>
+                    <img src='${userFrom.profilePic}'>
+                </div>
+                <div class='resultsDetailsContainer ellipsis'>
+                    <span ckass='ellipsis'>This is the text</span>
+                </div>
+            <a/>`
+}
