886732e6067cccdda5b78170dc6ef03e2bafffee
Vladislav Pestsov <pestsov.js@gmail.com>
2021-11-06 20:29:28 +0300

    creating the notification text

--- public/js/notificationsPage.js
+++ public/js/notificationsPage.js
@@ -16,15 +16,38 @@ function outputNotificationList(notifications, container) {
 }
 
 function createNotificationHtml(notification) {
-
     const userFrom = notification.userFrom;
+    const text = getNotificationText(notification)
 
     return `<a href='#' class='resultListItem notification'>
                 <div class='resultsImageContainer'>
                     <img src='${userFrom.profilePic}'>
                 </div>
                 <div class='resultsDetailsContainer ellipsis'>
-                    <span ckass='ellipsis'>This is the text</span>
+                    <span ckass='ellipsis'>${text}</span>
                 </div>
             <a/>`
+}
+
+function getNotificationText(notification) {
+
+    const userFrom = notification.userFrom
+    if (!userFrom.firstName || !userFrom.lastName) {
+        return alert("Пользователь не был запопулейтен")
+    }
+
+    const userFromName = `${userFrom.firstName} ${userFrom.lastName}`;
+    let text
+
+    if (notification.notificationType == "retweet") {
+        text = `${userFromName} ретвитнул один из Ваших постов` 
+    } else if (notification.notificationType == "like") {
+        text = `${userFromName} понравился один из Ваших постов` 
+    } else if (notification.notificationType == "reply") {
+        text = `${userFromName} репостнул один из Ваших постов` 
+    } else if (notification.notificationType == "follow") {
+        text = `${userFromName} подписался на Вас` 
+    }
+
+    return `<span class='ellipsis'>${text}</span>`
 }
