cdd8e505ddf7a96cb1e7a36007e6379fed28225c
Vladislav Pestsov <pestsov.js@gmail.com>
2022-04-10 18:11:15 +0300

    Update messagesRoutes.js

--- src/routes/messagesRoutes.js
+++ src/routes/messagesRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Chat = require("../schemas/ChatSchema");
+const Chat = require("../schemas/ChatSchema.js");
 const User = require("../schemas/UserSchema");
 const mongoose = require("mongoose");
 const router = express.Router();
